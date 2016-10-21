fbba184b682f9810c93feaf2206a909e7a0a6e2b
Marvin Winkler <marvambass@gmail.com>
2016-10-21 18:04:56 +0200

    first working and testet version for raspberry pi

--- eldopy.py
+++ eldopy.py
@@ -6,14 +6,14 @@ class Eldopy:
 
   # gpio true and false
   LOW = 0
-  HIGHT = 1
+  HIGH = 1
 
   # wireless true and false
   WTRUE = 136
   WFALSE = 142
 
-  # pulselenght is 300ms = 0.3 seconds
-  PULSE_LENGTH = 0.3
+  # pulselenght is 300 micoseconds = 0.0003 seconds (300.0 / 1000000.0)
+  PULSE_LENGTH = 0.0003
 
   # repeat transmission
   REPEAT = 10
@@ -22,28 +22,38 @@ class Eldopy:
 
   def __init__(self,gpio):
     self.gpio = gpio
-    file = open("/sys/class/gpio/export","w")
-    file.write(str(self.gpio))
-    file.close()
 
-    file = open("/sys/class/gpio/gpio" + str(self.gpio) + "/direction","w")
-    file.write("out")
-    file.close()
+    try:
+      file1 = open("/sys/class/gpio/export","w")
+      file1.write(str(self.gpio))
+      file1.close()
+    except:
+      #print("export failed")
+      pass
 
-    file = open("/sys/class/gpio/gpio" + str(self.gpio) + "/value","w")
-    file.write(str(self.LOW))
-    file.close()
+    try:
+      file2 = open("/sys/class/gpio/gpio" + str(self.gpio) + "/direction","w")
+      file2.write("out")
+      file2.close()
+    except:
+      #print("direction failed")
+      pass
+
+    self.digitalWrite(self.LOW)
 
 
 
   def digitalWrite(self, value):
-    file = open("/sys/class/gpio/gpio" + str(self.gpio) + "/value","w")
-    file.write(str(value))
-    file.close()
-
+    filehandle = open("/sys/class/gpio/gpio" + str(self.gpio) + "/value","w")
+    filehandle.write(str(value))
+    try:
+      filehandle.close()
+    except:
+      print("filehandle coudn't be closed")
 
 
   def sendEther(self, code):
+    filehandle = open("/sys/class/gpio/gpio" + str(self.gpio) + "/value","w")
     for r in range(0, self.REPEAT):
       for c in range(0, 16):
         x = 128
@@ -81,7 +91,7 @@ class Eldopy:
     for i in range(0, 5):
       if codeStr[i] == '1':
         code[i] = self.WTRUE
-      else
+      else:
         code[i] = self.WFALSE
 
     # parse device-id (A - E)
@@ -105,3 +115,17 @@ class Eldopy:
       code[11] = self.WTRUE
 
     self.sendEther(code)
+
+
+if __name__ == '__main__':
+  import sys
+  if len(sys.argv) == 4:
+    eldo = Eldopy(int(sys.argv[1]))
+    eldo.send433Mhz(sys.argv[2],(sys.argv[3] == 'True'))
+  else:
+    print("run this script with the following arguments:")
+    print("./eldopy.py gpioNumber AB440CodeString OnOffBoolean")
+    print("")
+    print("example:")
+    print("./eldopy.py 3 11011B True")
+    print("this will switch on 11011B via GPIO Pin 3")
